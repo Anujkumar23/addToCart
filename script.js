8661d1eab2ed9f6c7c425602139de6f76fa0076d
anuj <anujthakur315@gmail.com>
2022-10-16 21:14:39 +0530

    create the basic structure

---
+++ script.js
@@ -0,0 +1,110 @@
+products = [
+  {
+    id: 0,
+    name: "T-shirt ",
+    price: 200,
+    imgSrc: "001.jpg",
+    
+  },
+  {
+    id: 1,
+    name: "T-shirt ",
+    price: 150,
+    imgSrc: "002.jpg",
+  },
+    
+  {
+    id: 2,
+    name: "T-shirt ",
+    price: 250,
+    imgSrc: "003.jpg",
+    
+  },
+  {
+    id: 3,
+    name: "T-shirt ",
+    price: 250,
+    imgSrc: "004.jpg",
+     
+  },
+];
+
+let productElm = document.querySelector(".flex-left");
+let cartItemsElm = document.querySelector(".cart-items");
+let subtotalElm=document.querySelector(".subtotal")
+
+function renderProduct() {
+  products.forEach((product) => {
+    productElm.innerHTML += `
+    <div class="product-box">
+      <div class="img-box"id="">
+        <img src="${product.imgSrc}" alt="${product.name}" />
+      </div>
+
+      <section class="info">
+        <h4>T-shirt</h4>
+        <div class="price">
+          <strong>$ ${product.price}</strong>
+          </div>
+          </section>
+          <div class="button">
+            <button onclick="addToCart(${product.id})">Add To Cart</button>
+            </div>`;
+  });
+}
+renderProduct();
+
+let cart = [];
+function addToCart(id) {
+  // if product already exist in cart
+      if(cart.some((item)=>{
+        return item.id===id
+      })
+      ){
+        console.log("item already in cart")
+      }
+  else {
+    let item = products.find((product) => product.id === id);
+
+    cart.push(item);
+   
+  }
+
+  renderCartItems();
+  
+}
+
+function renderCartItems() {
+  cartItemsElm.innerHTML =""
+  cart.forEach((item) => {
+    console.log(item);
+    cartItemsElm.innerHTML += `
+     <div class="cart-item">
+     <div class="item-info column1">
+       <div class="img">
+         <img src="${item.imgSrc}" alt="t-shirt1">
+       </div>
+       <div class="name">
+         <h4>${item.name}</h4>
+       </div>
+     </div>
+       <div class="unit-price ">
+         $<span>${item.price}</span>
+       </div>
+       <div class="units ">
+        <input type="number" id="num" value=1 width=30%>
+        <button id="btn" onclick="removeCartItem(${item.id})">Remove</button>
+       </div>  
+</div>`;
+    
+  });
+}
+
+
+function removeCartItem(id) {
+  let index=cart.findIndex((item)=>item.id===id);
+ 
+  let removeItem=cart.splice(index,1);
+ 
+  renderCartItems();
+ }
